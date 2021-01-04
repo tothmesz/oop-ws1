13f23af4fe55421894c0d97c7a5e21cb9eebb518
tothmesz <66495712+tothmesz@users.noreply.github.com>
2021-01-04 14:20:09 +0100

    Make range safe (start always smaller than or equals than end)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args){
-        Range range1 = new Range(1, 2);
-        Range range2 = new Range(2, 3);
+        Range range1 = new Range(1, 12);
+        Range range2 = new Range(2, 8);
 
-        System.out.println(range1.start);
-        range1.start = 3;
-        range2.start = 5;
-        System.out.println(range1.start);
-        System.out.println(range2.start);
+
+
+        System.out.println(range1.getStart());
+        range1.setStart(3);
+        range2.setStart(5);
+        System.out.println(range1.getStart());
+        System.out.println(range2.getStart());
+
+        System.out.println(range1.length());
+        System.out.println(range2.length());
 
     }
 }
--- src/main/java/Range.java
+++ src/main/java/Range.java
@@ -1,14 +1,34 @@
 public class Range {
-    public int start;git a
-    public int end;
+    private int start;
+    private int end;
 
     public Range(int start, int end){
         this.start = start;
         this.end = end;
 
+        if (start > end){
+            throw new IndexOutOfBoundsException();
+        }
     }
 
+    public int getStart() {
+        return start;
+    }
 
+    public void setStart(int value) {
+        this.start = value;
+    }
 
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int value) {
+        this.end = value;
+    }
+
+    public int length(){
+        return this.end - this.start;
+    }
 
 }
