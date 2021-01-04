94735a7a98c465c3bb53ebfcc26b3d47ef1e2ed2
tothmesz <66495712+tothmesz@users.noreply.github.com>
2021-01-04 13:42:19 +0100

    Create start and end public fields Create 2 instances of range class and set its fields in Main.main()

--- src/main/java/Range.java
+++ src/main/java/Range.java
@@ -1,10 +1,10 @@
 public class Range {
-    public int start;
+    public int start;git a
     public int end;
 
     public Range(int start, int end){
-        this.start = 0;
-        this.end = 0;
+        this.start = start;
+        this.end = end;
 
     }
 
